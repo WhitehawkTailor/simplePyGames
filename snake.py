8b90b2bcefc72fb0715d6fa71eb7b5c18536d418
WhitehawkTailor <76742732+WhitehawkTailor@users.noreply.github.com>
2024-02-04 20:54:07 +0100

    Javítások ütközésben. Megjegyzések kiegészítése.

--- snake.py
+++ snake.py
@@ -1,10 +1,11 @@
 """
-Egyszerű ping-pong játék
+Az ismert snake játék
 """
 import tkinter as tk #Rajzolási eszkoz beemelése a kódba 0,0 a bal felső sarok
 import random
 
 #LÉPÉSEK
+#List és Dictionary fogalma használata. Ciklusban lista minden elemét lekérdezni, változtatni
 #Ablak létrehozása
 #jatek ciklus létrehozása - FPS fogalma - számol, töröl, kirajzol
 #Labda változó létrehozása
@@ -24,9 +25,8 @@ ablakMagassag = 600
 ablakSzelesseg = 1000
 meret=10
 
-#kukac={"szin":"green", "vx":-meret, "vy":0, "lepes":meret, "hossz":2, "vastagsag":meret, "test":[[500,300],[480,300]]}
-kukac={"szin":"green", "vx":-meret, "vy":0, "lepes":meret, "hossz":1, "vastagsag":meret, "test":[[500,300]]}
-alma={"x":100, "y":300, "latszik":0, "pont":1, "szin":"red", "meret":meret}
+kukac={}
+alma={}
 
 vege = False
 
@@ -42,6 +42,8 @@ def abs(arg):
 def billentyuMegnyomas(billentyu):
     global kukac, vege
     
+    #print(billentyu.keycode)
+
     if vege:
         kezdes()
         return
@@ -66,26 +68,28 @@ def billentyuMegnyomas(billentyu):
             kukac["vx"]=kukac["lepes"]
             kukac["vy"]=0
         
-    #print(billentyu.keycode)
+    
 
 
 def utkozes():
     global kukac, ablakMagassag, ablakSzelesseg, meret
     #fej koordináta
-    x = kukac["test"][0][0]
-    y = kukac["test"][0][1]
-    #pálya széle
-    if x<meret/2: return True
-    if y<meret/2: return True
-    if x>ablakSzelesseg-meret/2: return True
-    if y>ablakMagassag-meret/2: return True
+    x = kukac["test"][0][0] #0. cella 0. eleme
+    y = kukac["test"][0][1] #0. cella 1. eleme
+    #pálya szélével ütközik?    
+    if x<meret/2: return True #bal széle
+    if y<meret/2: return True #teteje
+    
+    if x>ablakSzelesseg-meret/2: return True #jobb széle
+    if y>ablakMagassag-meret/2: return True #alja
 
     #önmaga
     test=kukac["test"]
-    if kukac["hossz"]>1:
-        for i in range(1,kukac["hossz"]): #ütközik. e a fej a többi cellával
+    if kukac["hossz"]>4: #akkor kell csak önmaga ütközést vizsgálni, ha képes is rá
+        for i in range(1,kukac["hossz"]): #ütközik-e a fej másik cellával
             if abs(test[i][0] - x)<meret and abs(test[i][1] - y)<meret: return True
 
+    #ha nem talált ütközést
     return False    
     
 
@@ -109,7 +113,7 @@ def almaElkapas():
 def kezdes():
     global kukac, alma, vege
     kukac={"szin":"green", "vx":-meret, "vy":0, "lepes":meret, "hossz":1, "vastagsag":meret, "test":[[500,300]]}
-    alma={"x":100, "y":300, "latszik":0, "pont":1, "szin":"red", "meret":meret}
+    alma={"x":250, "y":300, "latszik":0, "pont":1, "szin":"red", "meret":meret}
     vege = False
     jatek()
 
@@ -120,61 +124,63 @@ def jatek():
     global c, alma, kukac, vege
 
     if vege:
-        c.create_text(500, 300, text="VÉGE", fill="white", font=('Helvetica 35 bold'))
-        #sc.after(50, jatek)#jatek eljárás újbóli meghívása 30ms múlva
+        c.create_text(500, 300, text="VÉGE", fill="white", font=('Helvetica 35 bold'))        
         return
 
-    test=kukac["test"]
-
     #mozgás
+    test=kukac["test"] #csak a testtel egyszerübb
     for i in range(kukac["hossz"]-1, -1, -1): #háturól kell kezdeni a ciklust
         if i==0: #fejet tegye az új pozícióba
             test[i][0]=test[i][0]+kukac["vx"]
             test[i][1]=test[i][1]+kukac["vy"]
-        else: #minden más az elötte lévő pozícióba menjen
+        else: #hátulról előre haladva minden az elötte lévő pozícióba menjen
             test[i][0]=test[i-1][0]
             test[i][1]=test[i-1][1]
 
+    #van-e ütközés?
     if utkozes():
         vege=True
 
+    #van-e alma elkapás?
     almaElkapas()
 
     #[KIRAJZOLÁS]
     c.delete('all') #korábbi tartalom törlése, új képkocka rajzolás megkezdéséhez
     
-    #Szöveg
+    #Szöveg - elsőként, hogy ne takarja az almat, vagy a kukacot
     c.create_text(500, 50, text=str( kukac["hossz"]-1), fill="yellow", font=('Helvetica 35 bold'))
 
-    #alma kirajzolasa
+    #alma kirajzolasa, az eltárolt koordináta az alma közepe
     c.create_rectangle(alma["x"]-alma["meret"]/2,
                        alma["y"]-alma["meret"]/2,
                        alma["x"]+alma["meret"]/2,
                        alma["y"]+alma["meret"]/2,
                        fill=alma["szin"])
 
-    #kukac kirajzolás
+    #kukac kirajzolás, az eltárolt koordináták a cellák közepe
     for i in range(0,kukac["hossz"]):
-        if i==0:
+        if i==0: #fej rajzolása eltér, teli cella
             c.create_rectangle(test[i][0]-kukac["vastagsag"]/2,
                        test[i][1]-kukac["vastagsag"]/2,
                        test[i][0]+kukac["vastagsag"]/2,
                        test[i][1]+kukac["vastagsag"]/2,
                        fill=kukac["szin"],
                        outline=kukac["szin"])
-        else:
+        else: #csak cella keret
             c.create_rectangle(test[i][0]-kukac["vastagsag"]/2,
                        test[i][1]-kukac["vastagsag"]/2,
                        test[i][0]+kukac["vastagsag"]/2,
                        test[i][1]+kukac["vastagsag"]/2,
+                       #fill=kukac["szin"],
                        outline=kukac["szin"])
-                       #fill=kukac["szin"])
+                       
 
 
     #A háttérben rajzolt tartalom megjelenítése
     sc.update() 
-    
-    sc.after(50, jatek)#jatek eljárás újbóli meghívása 30ms múlva
+
+    #jatek eljárás újbóli meghívása 50ms múlva - gyorsítás / lassítás
+    sc.after(50, jatek)
 
 
 
